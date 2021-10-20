1755492375912a3692172a91ab73b3a4d4d5342d
Maximiliano E. Lutz <maximiliano.lutz1997@gmail.com>
2021-10-20 17:56:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 
 import tkinter
 from tkinter import *
+from tkinter import ttk
 from tkinter import filedialog
 import os
 import random
+import json
 
 # Global Variables
 
@@ -16,11 +18,21 @@ movieResult = ""
 def close(x):
     x.destroy()
 
+def storeFolder(x):
+    filename = "folderPath.json"
+    try:
+        with open(filename, "w") as filePath:
+            json.dump(x, filePath)
+        print("Folder stored ----> SUCCESSFULL!")
+    except:
+        print("Folder stored ----> UNSUCCESSFULL!")
+
 # GUI Definitions
 
 def selectFolder(self): # Opens Windows file explorer and stores the file selected in a global variable.
     global xFolder
     xFolder = filedialog.askdirectory(title = "Select Folder")
+    storeFolder(xFolder)
 
 def customizeAppearance(self):
     print("Appearance")
@@ -29,61 +41,78 @@ def customizeSettings(self):
     print("Settings")
 
 def releaseNotes(self):
-    print("Release Notes")
-    # new top window without upper bar
-    # label with release notes
-    # scrollbar if release notes are an extensive .txt
-    # close button
-
-def reportProblem(self):
-    print("Report a problem")
-
-def aboutApplication(self):
-    aboutWindow = Toplevel()
+    releaseNotesWindow = Toplevel()
 
     "------------------------------------------------------"
 
     # Window bar
-    #aboutWindow.overrideredirect(True)
-    aboutWindow.title("About")
-    aboutWindow.resizable(0, 0)
+    #releaseNotesWindow.overrideredirect(True)
+    releaseNotesWindow.title("About")
+    releaseNotesWindow.resizable(0, 0)
 
     # Window size and position
     w = 400 # Width for the Tk window.
-    h = 600 # Height for the Tk window.
+    h = 525 # Height for the Tk window.
 
-    ws = aboutWindow.winfo_screenwidth() # Width of the screen.
-    hs = aboutWindow.winfo_screenheight() # Height of the screen.
+    ws = releaseNotesWindow.winfo_screenwidth() # Width of the screen.
+    hs = releaseNotesWindow.winfo_screenheight() # Height of the screen.
 
     # X and Y coordinates for the Tk window.
     x = (ws/2) - (w/2)
     y = (hs/2) - (h/2)
 
     # Dimensions of the screen and where it is placed.
-    aboutWindow.geometry('%dx%d+%d+%d' % (w, h, x, y))
+    releaseNotesWindow.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
     # Window background color
-    aboutWindow.configure(bg = "white")
+    releaseNotesWindow.configure(bg = "white")
 
-    "------------------------------------------------------"
+    " Top Frame -------------------------------------------"
 
-    # Application description ---------------------------- #
-    aboutLabel = Label(aboutWindow, text = "About application",
+    topFrame = Frame(releaseNotesWindow, bg = "white")
+    topFrame.pack()
+
+
+    releaseNotesTitleLabel = Label(topFrame, text = "Release Notes",
         bg = "white",
-        font = ("Courier New", 11)
+        font = ("Courier New", 30, "underline")
     )
-    aboutLabel.pack(
-        side = TOP,
-        padx = 10,
-        pady = (10, 0)
+    releaseNotesTitleLabel.pack(
+        pady = 5
     )
 
-    # Close application description window --------------- #
-    closeButton = Button(aboutWindow, text = "Close",
+    " Middle Frame ----------------------------------------"
+
+    middleFrame = Frame(releaseNotesWindow, bg = "white")
+    middleFrame.pack()
+
+    releaseNotesText = Text(middleFrame,
+        relief = FLAT)
+    releaseNotesText.configure(state = "disabled")
+    releaseNotesText.pack(
+        side = LEFT,
+        padx = 10,
+        pady = 10
+    )
+
+    textScrollbar = Scrollbar(middleFrame, orient = VERTICAL,
+        command = releaseNotesText.yview()
+    )
+    textScrollbar.pack(
+        side = RIGHT
+    )
+
+    " Bottom Frame ----------------------------------------"
+
+    bottomFrame = Frame(releaseNotesWindow, bg = "white")
+    bottomFrame.pack()
+
+    # Close release notes window --------------- #
+    closeButton = Button(bottomFrame, text = "Close",
         bg = "white",
         relief = SOLID,
         font = ("Courier New", 12),
-        command = lambda: close(aboutWindow)
+        command = lambda: close(releaseNotesWindow)
     )
     closeButton.pack(
         side = BOTTOM,
@@ -94,17 +123,31 @@ def aboutApplication(self):
 
     "------------------------------------------------------"
 
-    # Window MainLoop
-    aboutWindow.mainloop()
+    releaseNotesWindow.mainloop()
 
-def randomChoice(self): # From the previously selected folder, list all the subfolders there are and choose a random one.
+def reportProblem(self):
+    print("Report a problem")
+
+def appAbout(self):
+    print("About")
+
+def randomChoice(self):
     try:
-        movieFolder = os.listdir(xFolder)
-        for x in range(1):
-            movieResult = random.choice(movieFolder)
-            self.displayResultLabel.config(text = movieResult)
+        filename = "folderPath.json"
+        with open(filename) as filePath:
+            movieFolder = os.listdir(json.load(filePath))
+            for x in range(1):
+                movieResult = random.choice(movieFolder)
+                self.displayResultLabel.config(text = movieResult)
     except FileNotFoundError:
-        selectFolder(self)
+        try:
+            movieFolder = os.listdir(xFolder)
+            for x in range(1):
+                movieResult = random.choice(movieFolder)
+                self.displayResultLabel.config(text = movieResult)
+        except FileNotFoundError:
+            selectFolder(self)
 
-def playChoice(self): # Open the movie's folder and list all the files inside.
+def playChoice(self):
     print("Play selected movie")
+
